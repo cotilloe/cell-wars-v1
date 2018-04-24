bc3c47245a289c3d832f77a943a0747f05916e4f
Gene <eugene_cotillo@daytonastate.edu>
2018-04-24 10:23:00 -0400

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -699,7 +699,7 @@ function sendUpdates() {
                         f.cells[z].x-f.cells[z].radius < u.x + u.screenWidth/2 + 20 &&
                         f.cells[z].y+f.cells[z].radius > u.y - u.screenHeight/2 - 20 &&
                         f.cells[z].y-f.cells[z].radius < u.y + u.screenHeight/2 + 20) {
-                        z = f.cells.lenth;
+                        z = f.cells.length;
                         if(f.id !== u.id) {
                             return {
                                 id: f.id,
